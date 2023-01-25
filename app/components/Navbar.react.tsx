195345f5a3db7985c9f5e971b6dbea8ffd13f978
drifterz13 <mixmyxxe@gmail.com>
2023-01-25 07:13:12 +0700

    update nav

--- app/components/Navbar.react.tsx
+++ app/components/Navbar.react.tsx
@@ -1,53 +1,47 @@
-import { Bars3Icon } from '@heroicons/react/20/solid'
+import {
+  Bars3Icon,
+  MagnifyingGlassCircleIcon,
+  MagnifyingGlassIcon,
+} from '@heroicons/react/20/solid'
 import { Link, NavLink } from '@remix-run/react'
 import { clsx } from 'clsx'
 import { useState } from 'react'
 
 export default function Navbar() {
-  const [expand, setExpand] = useState(false)
-
   return (
-    <nav className='bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded'>
-      <div className='container flex flex-wrap items-center justify-between mx-auto'>
-        <Link to='/' className='flex items-center'>
-          <span className='self-center text-xl font-semibold whitespace-nowrap'>
-            Refin
-          </span>
+    <nav className='bg-white border-gray-200 mx-6 py-3'>
+      <div className='flex items-center justify-between w-full'>
+        <Link to='/'>
+          <span className='text-xl font-semibold whitespace-nowrap'>Refin</span>
         </Link>
-        <button
-          data-collapse-toggle='navbar-default'
-          type='button'
-          className='inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200'
-          aria-controls='navbar-default'
-          aria-expanded={expand}
-          onClick={() => setExpand((prevState) => !prevState)}
-        >
-          <span className='sr-only'>Open main menu</span>
-          <Bars3Icon className='w6 h-6' aria-hidden={!expand} />
-        </button>
-        <div
-          className={clsx(
-            'w-full md:w-auto',
-            expand ? 'block' : 'hidden md:block'
-          )}
-          id='navbar-default'
-        >
-          <ul className='flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white'>
-            <li>
-              <NavLink
-                to='/amc'
-                className={({ isActive }) =>
-                  clsx(
-                    'block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0',
-                    isActive &&
-                      'text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700'
-                  )
-                }
-              >
-                Fund AMC
-              </NavLink>
-            </li>
-          </ul>
+
+        <div className='flex items-center gap-5'>
+          <button className='p-2 w-48 flex text-sm items-baseline text-gray-500 bg-gray-50 border border-gray-300 rounded-lg'>
+            <MagnifyingGlassIcon className='w-3.5 h-3.5 mr-3' />
+            <span>Search</span>
+            <span className='ml-auto'>
+              <kbd>⌘</kbd>
+              <kbd>K</kbd>
+            </span>
+          </button>
+
+          <div>
+            <ul className='flex space-x-8 text-sm'>
+              <li>
+                <NavLink
+                  to='/amc'
+                  className={({ isActive }) =>
+                    clsx(
+                      'block py-2 pl-3 pr-4 text-gray-700 hover:bg-transparent hover:text-blue-700',
+                      isActive && 'text-blue-700'
+                    )
+                  }
+                >
+                  Fund AMC
+                </NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
